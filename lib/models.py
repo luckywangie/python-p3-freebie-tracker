dac9e85be8a5131897eda70a6419406cf0952242
lucky wangie <wangiemamati@gmail.com>
2025-05-24 10:29:58 +0300

    added new class in models.py and created relationships

--- lib/models.py
+++ lib/models.py
@@ -9,6 +9,9 @@ metadata = MetaData(naming_convention=convention)
 
 Base = declarative_base(metadata=metadata)
 
+#To do
+#to create a new cls modal
+#to add relations
 class Company(Base):
     __tablename__ = 'companies'
 
@@ -16,6 +19,8 @@ class Company(Base):
     name = Column(String())
     founding_year = Column(Integer())
 
+#relationship
+    freebies = relationship('Freebie', back_populates='company')
     def __repr__(self):
         return f'<Company {self.name}>'
 
@@ -25,5 +30,25 @@ class Dev(Base):
     id = Column(Integer(), primary_key=True)
     name= Column(String())
 
+#relationship
+    freebies = relationship('Freebie', back_populates='dev')
     def __repr__(self):
         return f'<Dev {self.name}>'
+
+#made a  new cls model
+class Freebie(Base):
+    __tablename__ = 'freebies'
+
+    id=Column(Integer(), primary_key=True)
+    item_name = Column(String())
+    value = Column(Integer())
+
+    company_id = Column(Integer(), ForeignKey('companies.id'))
+    dev_id = Column(Integer(), ForeignKey('devs.id'))
+
+#relationships
+    company = relationship('Company', back_populates='freebies')
+    dev = relationship('Dev', back_populates='freebies')
+
+    def __repr__(self):
+     return f'<Freebie {self.item_name} (${self.value})>'
